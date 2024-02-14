24e1d963b64c66f11c624d8ad64d8252024a0248
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2024-02-14 20:39:33 -0300

    feat: Iniciando desenvolvimento do auth

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import { Routes, Route } from "react-router-dom";
 import "./globals.css";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/forms/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 import { Home } from "./_root/pages";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
-        <Route path="/sign-in" element={<SigninForm />} />
-        <Route path="/sign-up" element={<SignupForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
