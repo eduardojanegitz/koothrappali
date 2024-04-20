a641354d0cf785affe39144b450ac3b559c5b97a
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2024-04-20 16:34:56 -0300

    feat: Atualizando HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,17 @@ import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/forms/AuthLayout";
 import RootLayout from "./_root/RootLayout";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -12,13 +22,23 @@ const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
+        {/* PUBLIC ROUTES */}
         <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SigninForm />} />
           <Route path="/sign-up" element={<SignupForm />} />
         </Route>
 
+        {/* PRIVATE ROUTES */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/posts/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
